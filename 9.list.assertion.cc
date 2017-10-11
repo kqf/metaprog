3192137885fb121ce8d6f2f65812f1e1bec37322
Sasha <q.dev.null@gmail.com>
2017-10-11 18:26:51 +0200

    Add list assertion

---
+++ 9.list.assertion.cc
@@ -0,0 +1,42 @@
+/******************************************************/
+
+// Helper: Will fail to compile if the assert is false
+class Assertion {};
+template <bool cond, class T = Assertion> struct Assert
+{
+    typedef typename T::fail check;
+};
+template <> struct Assert<true>
+{
+    typedef void check;
+};
+
+/******************************************************/
+
+template <typename Elm, typename LST> struct _Position
+{
+    typedef typename LST::Head Head;
+    typedef typename LST::Tail Tail;
+
+    static const bool found = Eq<Elm, Head>::result;
+    static const int result = (found) ? 0 : 1 + _Position<Elm, Tail>::result;
+};
+
+// The compiler will try to expand the position check
+// after NIL has been reached if this isn't here
+template <typename Elm> struct _Position<Elm, NIL>
+{
+    static const int result = 0;
+};
+
+template <typename Elm, typename LST> struct Position
+{
+    typedef typename Assert<Includes< Elm, LST >::result>::check include;
+    static const int result = _Position<Elm, LST>::result;
+};
+
+
+int main()
+{
+    
+}
